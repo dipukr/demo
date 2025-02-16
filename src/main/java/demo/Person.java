4e07ff52b85b3b0be06c017db4530aceef711b12
koomar_dipu <koomar.dipu@gmail.com>
2025-02-16 20:03:28 +0530

    chg

---
+++ src/main/java/demo/Person.java
@@ -0,0 +1,13 @@
+package demo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Person {
+
+	private String name;
+	private String email;
+	private String phone;
+	
+	
+}
